869ccf4a8d3f24d00b0ca27505808cada80b70e9
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2021-07-12 21:50:21 -0300

    criando aba de cadastro do produto

---
+++ frontend/src/pages/painel/produtoCadastro/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Subcontainer } from './styles';
+import { Card } from '../../../components';
+
+export default function ProdutoCadastro() {
+    return (
+        <Container className="container d-flex justify-content-center align-items-center flex-column">
+            <h4 className="fw-bold mb-5">Preencha os campos abaixo para cadastrar um novo produto:</h4>
+            <Card edicao={true} cadastro={true} />
+        </Container>
+    )
+}
