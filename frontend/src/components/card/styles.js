f29c7e31a6feb05a3b459bfc1fab7228ee211681
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2021-07-12 21:49:24 -0300

    finalizando funcionalidades do card

--- frontend/src/components/card/styles.js
+++ frontend/src/components/card/styles.js
@@ -6,8 +6,44 @@ export const Container = styled.div`
     border-radius: 5px;
     padding: 10px;
     border: 1px solid ${({theme}) => theme.black};
+    position: relative;
+    padding-bottom: 45px;
 
     fieldset {
         margin-bottom: 15px;
+        input {
+            transition: none;
+            &:disabled {
+                background: transparent;
+                border: none;
+                cursor: text;
+            }
+        }
+    }
+`;
+
+export const Button = styled.button`
+    width: 35px;
+    height: 35px;
+    background: #fff;
+    border-radius: 50%;
+    transition: all 200ms ease;
+    &:hover {
+        transform: scale(1.2);
+    }
+`;
+
+export const BoxPreco = styled.span`
+    border-radius: 3px;
+    background: #D4FFD9;
+    padding: 3px 8px;
+    color: ${({theme}) => theme.black};
+    bottom: -10px;
+    right: -10px;
+    position: absolute;
+    .valor {
+        padding: 0 20px;
+        font-family: 'Roboto Slab', serif;
+        font-size: 18px;
     }
 `;
