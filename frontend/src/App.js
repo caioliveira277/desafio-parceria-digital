26bfcbdba9a0d96319acbf840cc3507044118770
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2021-07-12 21:48:34 -0300

    implementando rotas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { GlobalStyle, theme } from './theme/global';
 import { ThemeProvider } from 'styled-components';
-import Painel from './pages/painel';
+import Routes from './routes/routes';
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Painel />
+      <Routes />
     </ThemeProvider>
   )
 }
