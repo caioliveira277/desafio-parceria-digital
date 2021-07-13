804d644b10c901955c4ff0b58ef86e5b66c22aee
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2021-07-12 21:50:04 -0300

    exibindo aba atual com base na url

--- frontend/src/pages/painel/index.js
+++ frontend/src/pages/painel/index.js
@@ -1,18 +1,27 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import {
     Header,
     Nav 
 } from '../../components';
-import ProdutoLista from './produtoLista';
 import { Main } from './styles';
+import ProdutoLista from './produtoLista';
+import ProdutoCadastro from './produtoCadastro';
 
 export default function Painel() {
+    const {pathname} = useLocation();
     return (
         <>
             <Header />
             <Nav />
             <Main className="mt-4">
-                <ProdutoLista />
+                {
+                    pathname === '/produtos/cadastrar' ? (
+                        <ProdutoCadastro />
+                    ) : (
+                        <ProdutoLista />
+                    )
+                }
             </Main>
         </>
     )
